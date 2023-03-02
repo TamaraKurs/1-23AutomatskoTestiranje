ffa959c42724c51d9cd2e36ce9c922daaafb5437
tamara.mitrovic <tamara.mitrovic@panonit.com>
2023-03-02 14:50:07 +0100

    Changes

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Test")
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome()
+
+navigationPage = driver.get("https://www.google.com")
+inputField = driver.find_element("xpath", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys("SmartInIt")
