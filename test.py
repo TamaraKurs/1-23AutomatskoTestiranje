385ba8bee8b047777f12fda8d7c6dcabe894ada1
tamara.mitrovic <tamara.mitrovic@panonit.com>
2023-03-02 15:29:45 +0100

    no message

--- test.py
+++ test.py
@@ -6,5 +6,8 @@ import time
 
 driver = webdriver.Chrome()
 
+#navigacija na stranicu
 navigationPage = driver.get("https://www.google.com")
+
+#unos u polje
 inputField = driver.find_element("xpath", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys("SmartInIt")
