917396ee7ee53196393ef56e5f6f651a8acd00e9
tamara.mitrovic <tamara.mitrovic@panonit.com>
2023-03-07 17:48:23 +0100

    Updates

--- vezba2.py
+++ vezba2.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Chrome()
+
+#navigacija na stranicu
+navigationPage = driver.get("https://elearn.smartinit.net/login/index.php")
+
+#unos u polje
+inputField = driver.find_element("id", "username").send_keys("tamara.mitrovic")
+passwordField = driver.find_element("id","password").send_keys("")
+logInButton = driver.find_element("id","loginbtn").click()
